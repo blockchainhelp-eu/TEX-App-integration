6d650acb93f573241bc70c9bc6f217e80994e897
Blockchain Help <31084431+blockchainhelp@users.noreply.github.com>
2018-08-29 22:46:16 +0530

    Add files via upload

---
+++ racket-application.rkt
@@ -0,0 +1,5 @@
+#lang racket/base
+
+(require (only-in "lang-anarki-library.rkt" anarki-library-export))
+
+(displayln (string-append "Hello " anarki-library-export))
---
+++ racket-deep-dependency.rkt
@@ -0,0 +1,5 @@
+#lang racket/base
+
+(provide racket-deep-dependency-export)
+
+(define racket-deep-dependency-export "Racket")
